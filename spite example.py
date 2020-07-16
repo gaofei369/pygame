99c2afa4f810eeb8124a764dcbd22cc81dd7ddb2
gaofei369 <gaof595@gmail.com>
2020-07-17 00:09:30 +0800

    less3

--- spite example.py
+++ spite example.py
@@ -13,7 +13,7 @@ BLACK = (0,0,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
-
+YELLOW = (255,255,0)
 
 pygame.init()
 pygame.mixer.init()
@@ -44,6 +44,11 @@ class Player(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -63,12 +68,27 @@ class Mob(pygame.sprite.Sprite):
             self.rect.y = random.randrange(-100,-40)
             self.speedy = random.randrange(1,8)
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y +=self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
-palyer = Player()
-all_sprites.add(palyer)
-for i in range(8):
+bullets = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+for i in range    (8):      
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
@@ -82,10 +102,29 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
-    #Update
+    #Update              
     # print("test1")
     all_sprites.update()
+
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        m = Mob ()
+        all_sprites.add(m)
+        mobs.add(m)
+    
+
+
+
+
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits:
+        running = False
+
+
 
     #Draw / rendrer
     screen.fill(BLACK)
