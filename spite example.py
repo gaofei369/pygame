ebff94aac955bd6e85cab0d28d40e6d743ea656b
gaofei369 <gaof595@gmail.com>
2020-07-16 20:36:30 +0800

    less2

--- spite example.py
+++ spite example.py
@@ -3,8 +3,8 @@ import random
 import time
 import os
 
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 360
+HEIGHT = 480
 FPS = 30
 
 # define colors
@@ -14,34 +14,65 @@ RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 
-# set up assets folders
-game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder,"img")
-print(img_folder)
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.path.abspath(os.path.join(img_folder, "p2.png")))
-        # self.image = pygame.image.load('p1.png')
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-
-    def update(self):
-        self.rect.x += 5
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
-
 
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("My Ggame")
+pygame.display.set_caption("Shmup!")
 clock = pygame.time.Clock()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 5
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100,-40)
+        self.speedy = random.randrange(1,8)
+        self.speedx = random.randrange(-3,3)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left< -25 or self.rect.right >WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100,-40)
+            self.speedy = random.randrange(1,8)
+
+
 all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
 palyer = Player()
 all_sprites.add(palyer)
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
 
 # Game loop
 running = True
@@ -53,6 +84,7 @@ while running:
             running = False
 
     #Update
+    # print("test1")
     all_sprites.update()
 
     #Draw / rendrer
@@ -60,7 +92,7 @@ while running:
     all_sprites.draw(screen)
     pygame.display.flip()
 
-pygame.QUIT()
+pygame.quit()
 
 
 
