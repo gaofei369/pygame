27960516fe901cd4baa2df60a9fdafccba3605b4
gaofei369 <gaof595@gmail.com>
2020-07-19 19:40:51 +0800

    less6

--- spite example.py
+++ spite example.py
@@ -33,7 +33,7 @@ class Player(pygame.sprite.Sprite):
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.radius = 20
-        pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
@@ -59,22 +59,38 @@ class Player(pygame.sprite.Sprite):
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = meteor_img
-        self.rect = self.image.get_rect()
-        self.image.set_colorkey(BLACK)
-        self.radius = int(self.rect.width *  .9 / 2)
-        pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.image_orig = random.choice(meteor_images)
+        self.rect = self.image_orig.get_rect()
+        self.image = self.image_orig.copy()
+        self.image_orig.set_colorkey(BLACK)
+        self.radius = int(self.rect.width * .85 / 2)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100,-40)
         self.speedy = random.randrange(1,8)
         self.speedx = random.randrange(-3,3)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
+    
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update >50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
 
     def update(self):
+        self.rotate()
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.top > HEIGHT + 10 or self.rect.left< -25 or self.rect.right >WIDTH + 20:
             self.rect.x = random.randrange(WIDTH - self.rect.width)
-            self.rect.y = random.randrange(-100,-40)
+            self.rect.y = random.randrange(-150,100)
             self.speedy = random.randrange(1,8)
 
 class Bullet(pygame.sprite.Sprite):
@@ -95,11 +111,12 @@ class Bullet(pygame.sprite.Sprite):
 background = pygame.image.load(path.join(img_dir, "blue.png").replace('\\','/')).convert()
 background_rect = background.get_rect()
 player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.png").replace('\\','/')).convert()
-meteor_img = pygame.image.load(path.join(img_dir, "meteorBrown_big4.png").replace('\\','/')).convert()
 bullet_img = pygame.image.load(path.join(img_dir, "laserRed01.png").replace('\\','/')).convert()
-
-
-
+meteor_images = []
+meteor_list = ['meteorBrown_big1.png', 'meteorBrown_big2.png', 'meteorBrown_big3.png', 'meteorBrown_big4.png', 'meteorBrown_med1.png', 
+               'meteorBrown_med3.png', 'meteorBrown_small1.png', 'meteorBrown_small2.png', 'meteorBrown_tiny1.png', 'meteorBrown_tiny2.png']
+for img in meteor_list:
+    meteor_images.append(pygame.image.load(path.join(img_dir, img).replace('\\','/')).convert())
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
@@ -138,7 +155,7 @@ while running:
 
 
 
-    hits = pygame.sprite.spritecollide(player, mobs, False)
+    hits = pygame.sprite.spritecollide(player, mobs, pygame.sprite.collide_circle)
     if hits:
         running = False
 
